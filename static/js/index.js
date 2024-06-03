e0d6d359cc2eb91a20f6d0ce8d5c025d4c1f1f48
jiangMo09 <mmiao0641@gmail.com>
2024-06-04 03:02:36 +0800

    [FEAT]week1

---
+++ static/js/index.js
@@ -0,0 +1,151 @@
+let nextPage = 0;
+let loading = false;
+let defaultKeyword = "";
+
+const appendAttraction = (attraction, attractionsDiv) => {
+  const attractionDiv = document.createElement("div");
+  attractionDiv.classList.add("attraction");
+
+  const imageDiv = document.createElement("div");
+  imageDiv.classList.add("image");
+  imageDiv.style.backgroundImage = `url("${attraction.images[0]}")`;
+
+  const nameDiv = document.createElement("div");
+  nameDiv.classList.add("name");
+  nameDiv.textContent = attraction.name;
+
+  const infoDiv = document.createElement("div");
+  infoDiv.classList.add("info");
+
+  const mrtSpan = document.createElement("span");
+  mrtSpan.classList.add("mrt");
+  mrtSpan.textContent = attraction.mrt;
+
+  const categorySpan = document.createElement("span");
+  categorySpan.classList.add("category");
+  categorySpan.textContent = attraction.category;
+
+  imageDiv.appendChild(nameDiv);
+  infoDiv.appendChild(mrtSpan);
+  infoDiv.appendChild(categorySpan);
+  attractionDiv.appendChild(imageDiv);
+  attractionDiv.appendChild(infoDiv);
+
+  attractionsDiv.appendChild(attractionDiv);
+};
+
+const getAttractions = (keyword) => {
+  const attractionsDiv = document.querySelector(".attractions");
+
+  if (keyword) {
+    nextPage =
+      defaultKeyword === "" || defaultKeyword !== keyword ? 0 : nextPage;
+
+    if (defaultKeyword != keyword) {
+      attractionsDiv.innerHTML = "";
+    }
+
+    defaultKeyword = keyword;
+  }
+
+  const apiPath = keyword
+    ? `/api/attractions?page=${nextPage}&keyword=${keyword}`
+    : `/api/attractions?page=${nextPage}`;
+
+  loading = true;
+  fetch(apiPath)
+    .then((response) => response.json())
+    .then((data) => {
+      if (!data || data?.data?.length === 0) {
+        alert("沒有景點，請更換關鍵字");
+        return;
+      }
+
+      nextPage = data.nextPage;
+      data.data.forEach((attraction) => {
+        appendAttraction(attraction, attractionsDiv);
+      });
+    })
+    .catch((error) => {
+      console.error("Error fetching attractions:", error);
+    })
+    .finally(() => {
+      loading = false;
+    });
+};
+
+const getMRTs = () => {
+  fetch("/api/mrts")
+    .then((response) => response.json())
+    .then((data) => {
+      const mrtsDiv = document.querySelector(".mrts");
+
+      if (data && data.data && data.data.length > 0) {
+        data.data.forEach((mrtName) => {
+          const mrtSpan = document.createElement("span");
+          mrtSpan.classList.add("mrt");
+          mrtSpan.textContent = mrtName;
+          mrtSpan.addEventListener("click", function () {
+            getAttractions(mrtName);
+          });
+
+          mrtsDiv.appendChild(mrtSpan);
+        });
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching MRTs:", error);
+    });
+};
+
+const onSearchBtnClick = () => {
+  const inputElement = document.querySelector(".search");
+  const searchValue = inputElement.value;
+
+  if (!searchValue || searchValue.trim() === "") {
+    alert("請輸入關鍵字");
+    return;
+  }
+
+  getAttractions(searchValue);
+};
+
+const attachEventListeners = () => {
+  const mrtList = document.querySelector(".mrt-list");
+  const mrts = mrtList.querySelector(".mrts");
+  const arrowLeft = mrtList.querySelector(".arrow-left");
+  const arrowRight = mrtList.querySelector(".arrow-right");
+
+  arrowLeft.addEventListener("click", function () {
+    mrts.scrollBy({
+      left: -1000,
+      behavior: "smooth"
+    });
+  });
+
+  arrowRight.addEventListener("click", function () {
+    mrts.scrollBy({
+      left: 1000,
+      behavior: "smooth"
+    });
+  });
+
+  window.addEventListener("scroll", () => {
+    if (loading || !nextPage) {
+      return;
+    }
+
+    if (
+      window.innerHeight + window.scrollY >=
+      document.body.offsetHeight - 300
+    ) {
+      getAttractions(defaultKeyword);
+    }
+  });
+};
+
+window.addEventListener("load", function () {
+  getMRTs();
+  getAttractions();
+  attachEventListeners();
+});
