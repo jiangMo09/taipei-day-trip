eb00d59aafbdf42234a9725fc57c9343e996feeb
jiangMo09 <mmiao0641@gmail.com>
2024-06-18 10:32:06 +0800

    [chore]js 檔案分離

--- static/js/index.js
+++ static/js/index.js
@@ -155,66 +155,6 @@ const attachEventListeners = () => {
   });
 };
 
-const loginRegister = document.getElementById("login-register");
-const loginDialog = document.getElementById("loginDialog");
-
-loginRegister.onclick = function () {
-  loadLoginForm();
-};
-
-function loadLoginForm() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      loginDialog.innerHTML = this.responseText;
-      loginDialog.style.display = "block";
-
-      let closeBtn = document.getElementsByClassName("close")[0];
-      closeBtn.onclick = function () {
-        loginDialog.style.display = "none";
-      };
-      window.onclick = function (event) {
-        if (event.target == loginDialog) {
-          loginDialog.style.display = "none";
-        }
-      };
-
-      const signupLink = document.getElementsByClassName("signup")[0];
-      signupLink.onclick = function () {
-        loadSignupForm();
-      };
-    }
-  };
-  xhttp.open("GET", "/static/singin.html", true);
-  xhttp.send();
-}
-
-function loadSignupForm() {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      loginDialog.innerHTML = this.responseText;
-
-      const closeBtn = document.getElementsByClassName("close")[0];
-      closeBtn.onclick = function () {
-        loginDialog.style.display = "none";
-      };
-      window.onclick = function (event) {
-        if (event.target == loginDialog) {
-          loginDialog.style.display = "none";
-        }
-      };
-
-      const signinLink = document.getElementsByClassName("signin")[0];
-      signinLink.onclick = function () {
-        loadLoginForm();
-      };
-    }
-  };
-  xhttp.open("GET", "/static/signup.html", true);
-  xhttp.send();
-}
-
 window.addEventListener("load", function () {
   getMRTs();
   getAttractions();
